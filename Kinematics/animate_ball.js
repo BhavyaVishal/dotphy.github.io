3c7b06205c7822114edb3ddf2bb8f031bea31c2b
Aaditay <mail>
2020-08-27 22:31:54 +0530

    UI Update

---
+++ Kinematics/animate_ball.js
@@ -0,0 +1,89 @@
+let action = document.querySelector("#action");
+let reset = document.querySelector("#reset");
+
+let velocityInp = document.querySelector("#velocity-inp");
+let accInp = document.querySelector("#acc-inp");
+
+let velocityParam = document.querySelector("#velocity-param");
+let accParam = document.querySelector("#acc-param");
+
+let scaleInp = document.querySelector("#scale").querySelector("input");
+
+action.onclick = do_experiment;
+
+function do_experiment() {
+  let width = 50;
+  let velocity = velocityInp.value;
+  let acc = accInp.value;
+  let scale = scaleInp.value;
+
+  //------- Default Values for parameters ------
+  if (velocity == "") {
+    velocity = 1;
+  }
+  if (acc == "") {
+    acc = -9.8;
+  }
+  if (scale == "") {
+    scale = 10;
+  }
+
+  //---------------------------------------------
+
+  velocity = (velocity * scale) / 1000; // Velocity in px/ms
+  acc = (acc * scale) / 1000000; // Acceleration in px/ms*2
+  let distance = -(velocity * velocity) / (2 * acc);
+  console.log(distance);
+
+  let goUp = setInterval(moveUp, 1);
+  let goDwn = setInterval(moveDwn, 1);
+  let pos = 400;
+  let pos2 = 5;
+  function moveUp() {
+    reset.addEventListener("click", function () {
+      ball.style.top = "400px";
+      velocityParam.innerHTML = "0";
+      accParam.innerHTML = "0";
+      clearInterval(goUp);
+    });
+    accParam.textContent = String((acc * 100000).toFixed(2));
+    velocityParam.textContent = String((velocity * 100).toFixed(2));
+    let curr_distance = 0;
+    velocity = velocity + acc;
+    pos = pos - velocity;
+
+    if (pos < 5) {
+      width = width - velocity / 100;
+
+      ball.style.width = String(width) + "px";
+      ball.style.height = String(width) + "px";
+    } else if (pos > 401) {
+      clearInterval(moveUp);
+    } else {
+      curr_distance += curr_distance + velocity;
+      console.log(curr_distance * 10);
+      ball.style.top = String(pos) + "px";
+    }
+  }
+  function moveDwn() {
+    reset.addEventListener("click", function () {
+      ball.style.top = "400px";
+
+      clearInterval(goDwn);
+    });
+    velocity = velocity + acc;
+    pos = pos - velocity;
+
+    if (pos < 5) {
+      width = width - velocity / 100;
+
+      ball.style.width = String(width) + "px";
+      ball.style.height = String(width) + "px";
+    } else if (pos > 401) {
+      velocityParam.textContent = velocity.toFixed(2);
+      clearInterval(moveUp);
+    } else {
+      ball.style.top = String(pos) + "px";
+    }
+  }
+}
